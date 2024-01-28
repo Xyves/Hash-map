5578960e630cef7074d86a5a6cf328c89f95f241
Xyves <xodrixx@protonmail.com>
2024-01-28 19:21:41 +0100

    Implement all methods to hashmap

--- index.js
+++ index.js
@@ -1,55 +1,157 @@
-class Node {
-  constructor(name, surname) {
-    this.name = name;
-    this.surname = surname;
-  }
-}
-class linkedList {
-  constructor() {
-    this.head = null;
-    this.length = 0;
-  }
-}
 class hashMap {
   constructor() {
-    this.defaultCapacity = 16;
-    this.buckets = new Array(this.defaultCapacity);
+    this.defaultCapacity = 15;
+    this.buckets = new Array(this.defaultCapacity).fill([]);
+    this.keys = 0;
   }
-  hash(name, surname) {
+  hash(value) {
     function stringToNumber(string) {
-      console.log("Run");
       let hashCode = 0;
       let primeNumber = 31;
       for (let i = 0; i < string.length; i++) {
         hashCode = primeNumber * hashCode + string.charCodeAt(i);
         hashCode = hashCode % 5;
       }
-      console.log(hashCode);
       return hashCode;
     }
-    console.log(stringToNumber(name));
-    console.log("Doesn");
-    return stringToNumber(name) + stringToNumber(surname);
+    return stringToNumber(value);
   }
 
-  set(name, surname) {
-    const hashedVal = hash(name, surname);
-    let fullName = new Node(name, surname);
-    let objName = { name: fullName };
-
-    console.log(hashedVal);
-    this.buckets.splice(hashedVal, 0, objName);
+  set(key, value) {
+    let hashedVal = this.hash(value);
+    let newNode = new Node(key, value);
+    this.buckets[hashedVal] = [...this.buckets[hashedVal], newNode];
+    this.keys++;
   }
-  get() {}
-  has() {}
-  remove() {}
-  length() {}
-  clear() {}
-  keys() {}
-  values() {}
-  entries() {}
+  get(key1) {
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (this.buckets[i] != undefined) {
+        const foundNode = this.buckets[i].find((node) =>
+          node.hasOwnProperty(key1)
+        );
+        if (foundNode) {
+          let value = Object.values(foundNode);
+          console.log(value);
+          return value;
+        }
+      }
+    }
+    console.log("Undefined");
+    return undefined;
+  }
+  has(key1) {
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (this.buckets[i] && this.buckets[i].length != 0) {
+        const keyFound = this.buckets[i].some((object) =>
+          object.hasOwnProperty(key1)
+        );
+        if (keyFound) {
+          console.log("True " + key1 + " is found");
+          return true;
+        } else {
+          console.log("False " + key1 + " is not found");
+          return false;
+        }
+      }
+    }
+  }
+  remove(removeKey) {
+    for (let i = 0; i < this.buckets.length; i++) {
+      if (this.buckets[i].length !== 0) {
+        this.buckets[i] = this.buckets[i].filter((obj) => {
+          if (removeKey in obj) {
+            console.log("Deleting:", obj);
+            this.keys--;
+            return false;
+          }
+          return true;
+        });
+      }
+    }
+  }
+  length() {
+    console.log("Length = " + this.keys);
+  }
+  clear() {
+    this.buckets = new Array(this.defaultCapacity).fill([]);
+    this.keys = 0;
+    console.log(this.buckets);
+  }
+  getKeys() {
+    let storedKeys = [];
+    for (let array of this.buckets) {
+      if (array.length != 0) {
+        console.log("XD");
+        for (let obj of array) {
+          console.log("!");
+          for (let key in obj) {
+            storedKeys.push(key);
+          }
+        }
+      }
+    }
+    console.log(storedKeys);
+  }
+  values() {
+    let storedValues = [];
+    for (let array of this.buckets) {
+      if (array.length != 0) {
+        for (let obj of array) {
+          for (const key in obj) {
+            storedValues.push(obj[key]);
+          }
+        }
+      }
+    }
+    console.log(storedValues);
+  }
+  entries() {
+    let storedObj = [];
+    for (let array of this.buckets) {
+      if (array.length != 0) {
+        for (let obj of array) {
+          for (const key in obj) {
+            storedObj.push([key, obj[key]]);
+          }
+        }
+      }
+    }
+    console.log(storedObj);
+  }
 }
-
+class Node {
+  constructor(key, value) {
+    this[key] = value;
+  }
+}
 const newHashMap = new hashMap();
 
-newHashMap.hash("Pozdro", "Poćwicz");
+newHashMap.hash("City", "Stavanger");
+// Set objects
+newHashMap.set("car", "Ferrari");
+newHashMap.set("mountain", "Fuji");
+newHashMap.set("piece", "Queen");
+
+// Print value of key(car)
+newHashMap.get("car");
+
+// Check if the key exists
+newHashMap.has("Ra9");
+
+// Remove object
+// newHashMap.remove("car");
+
+// Print all keys
+// newHashMap.getKeys();
+
+// Print all values
+// newHashMap.values();
+
+// Print keys+ values
+// newHashMap.entries();
+
+// removes all entries in the hash map.
+// newHashMap.clear();
+
+// returns the number of stored keys in the hash map.
+// newHashMap.length();
